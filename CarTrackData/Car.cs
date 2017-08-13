543fa17a48fd85090808ffb9f1617bc7625fa539
A.K <adrian.kowalski87@gmail.com>
2017-08-13 18:22:05 +0200

    initial commit

---
+++ CarTrackData/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTrackData
+{
+    public class Car
+    {
+        string Brand;
+        string Model;
+        int YearMade;
+        long Mileage;
+        decimal Engine;
+
+        public Car(string Brand, string Model, int YearMade, long Mileage, decimal Engine) {
+            this.Brand = Brand;
+            this.Model = Model;
+            this.YearMade = YearMade;
+            this.Mileage = Mileage;
+            this.Engine = Engine;
+        }
+    }
+}
